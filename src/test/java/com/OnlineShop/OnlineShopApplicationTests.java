9e797abb610a9b8bdbe73590ba4eaf4e16811e04
farbod-behnam <farbod.behnam@gmail.com>
2022-04-26 23:10:07 +0430

    CategoryControllerTest has been initiated after a lot of research

--- src/test/java/com/OnlineShop/OnlineShopApplicationTests.java
+++ src/test/java/com/OnlineShop/OnlineShopApplicationTests.java
@@ -4,10 +4,12 @@ import com.OnlineShop.entity.Product;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Objects;
 
@@ -16,29 +18,11 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class OnlineShopApplicationTests {
 
-	@Autowired
-	private TestRestTemplate restTemplate;
-
 
 
 	@Test
 	@Disabled
 	void contextLoads() {
-	}
-
-
-	@Test
-	public void getProduct_returnsProductDetails()
-	{
-		// given
-
-		// when
-		ResponseEntity<Product> response= restTemplate.getForEntity("/api/products/1", Product.class);
-
-		// then
-		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-		assertThat(Objects.requireNonNull(response.getBody()).getName()).isEqualTo("Bloodborne");
-		assertThat(Objects.requireNonNull(response.getBody()).getCategory().name()).isEqualTo("Video_Games");
 	}
 
 }
---
+++ src/test/java/com/OnlineShop/controller/CategoryControllerTest.java
@@ -0,0 +1,35 @@
+package com.OnlineShop.controller;
+
+import com.OnlineShop.entity.Product;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.Objects;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class CategoryControllerTest
+{
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void getProducts_returnsOK() throws Exception
+    {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/categories"))
+                .andExpect(status().isOk());
+    }
+
+}
