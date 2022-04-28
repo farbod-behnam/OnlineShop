5ba87da6376640ee932f3f0740517584c4859385
farbod-behnam <farbod.behnam@gmail.com>
2022-04-28 12:02:14 +0430

    Create TDD for CategoryController.getCategories method

--- src/main/java/com/OnlineShop/controller/CategoryController.java
+++ src/main/java/com/OnlineShop/controller/CategoryController.java
@@ -1,10 +1,32 @@
 package com.OnlineShop.controller;
 
+import com.OnlineShop.entity.Category;
+import com.OnlineShop.service.CategoryService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/categories")
 public class CategoryController
 {
+    private final CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService)
+    {
+        this.categoryService = categoryService;
+    }
+
+
+    @GetMapping
+    public ResponseEntity<List<Category>> getCategories()
+    {
+        List<Category> categories = categoryService.findAll();
+
+        return new ResponseEntity<>(categories, HttpStatus.OK);
+    }
 }
--- src/test/java/com/OnlineShop/controller/CategoryControllerTest.java
+++ src/test/java/com/OnlineShop/controller/CategoryControllerTest.java
@@ -1,35 +1,76 @@
 package com.OnlineShop.controller;
 
-import com.OnlineShop.entity.Product;
+import com.OnlineShop.entity.Category;
+import com.OnlineShop.service.CategoryService;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import java.util.Objects;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+@ExtendWith(SpringExtension.class)
+@WebMvcTest
 public class CategoryControllerTest
 {
 
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    private CategoryService categoryService;
+
+
     @Test
-    public void getProducts_returnsOK() throws Exception
+    public void getCategories_returnsEmptyCategoryList() throws Exception
     {
+        // given
+        List<Category> categoryList = new ArrayList<>();
+
+        given(categoryService.findAll()).willReturn(categoryList);
+
+        // when
         mockMvc.perform(MockMvcRequestBuilders.get("/api/categories"))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").isArray())
+                .andExpect(jsonPath("$.length()").value(categoryList.size()));
+
+        // then
+//        verify(categoryService, times(1)).findAll();
+    }
+    @Test
+    public void getCategories_returnsCategoryList() throws Exception
+    {
+        // given
+        List<Category> categoryList = new ArrayList<>();
+        categoryList.add(new Category("11", "Video Games", null));
+        categoryList.add(new Category("12", "Clothes", null));
+
+        given(categoryService.findAll()).willReturn(categoryList);
+
+        // when
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/categories"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").isArray())
+                .andExpect(jsonPath("$.length()").value(categoryList.size()))
+                .andExpect(jsonPath("$[0].id").value(equalTo("11")))
+                .andExpect(jsonPath("$[0].name").value(equalTo("Video Games")));
+
+        // then
+//        verify(categoryService, times(1)).findAll();
     }
 
 }
