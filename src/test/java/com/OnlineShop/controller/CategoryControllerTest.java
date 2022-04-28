ce93f8d44870092d59c6bc688957b24c9f01b34a
farbod-behnam <farbod.behnam@gmail.com>
2022-04-28 20:01:57 +0430

    Fix BigDecimal comparison error (line #174) by setting up a config in @BeforeEach

--- src/test/java/com/OnlineShop/controller/CategoryControllerTest.java
+++ src/test/java/com/OnlineShop/controller/CategoryControllerTest.java
@@ -1,7 +1,17 @@
 package com.OnlineShop.controller;
 
 import com.OnlineShop.entity.Category;
+import com.OnlineShop.entity.Product;
 import com.OnlineShop.service.CategoryService;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jayway.jsonpath.Configuration;
+import com.jayway.jsonpath.Option;
+import com.jayway.jsonpath.spi.json.JacksonJsonProvider;
+import com.jayway.jsonpath.spi.json.JsonProvider;
+import com.jayway.jsonpath.spi.mapper.JacksonMappingProvider;
+import com.jayway.jsonpath.spi.mapper.MappingProvider;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,8 +24,12 @@ import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.EnumSet;
 import java.util.List;
+import java.util.Set;
 
 import static org.hamcrest.Matchers.equalTo;
 import static org.mockito.BDDMockito.given;
@@ -33,6 +47,35 @@ public class CategoryControllerTest
     @MockBean
     private CategoryService categoryService;
 
+    @BeforeEach
+    void setUp()
+    {
+
+        final ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.enable(DeserializationFeature.USE_LONG_FOR_INTS);
+        objectMapper.enable(DeserializationFeature.USE_BIG_DECIMAL_FOR_FLOATS);
+
+        Configuration.setDefaults(new Configuration.Defaults() {
+
+            private final JsonProvider jsonProvider = new JacksonJsonProvider(objectMapper);
+            private final MappingProvider mappingProvider = new JacksonMappingProvider(objectMapper);
+
+            @Override
+            public JsonProvider jsonProvider() {
+                return jsonProvider;
+            }
+
+            @Override
+            public MappingProvider mappingProvider() {
+                return mappingProvider;
+            }
+
+            @Override
+            public Set<Option> options() {
+                return EnumSet.noneOf(Option.class);
+            }
+        });
+    }
 
     @Test
     public void getCategories_returnsEmptyCategoryList() throws Exception
@@ -51,6 +94,8 @@ public class CategoryControllerTest
         // then
 //        verify(categoryService, times(1)).findAll();
     }
+
+
     @Test
     public void getCategories_returnsCategoryList() throws Exception
     {
@@ -69,6 +114,64 @@ public class CategoryControllerTest
                 .andExpect(jsonPath("$[0].id").value(equalTo("11")))
                 .andExpect(jsonPath("$[0].name").value(equalTo("Video Games")));
 
+        // then
+//        verify(categoryService, times(1)).findAll();
+    }
+
+    @Test
+    public void getCategories_returnsCategoryListWithProducts() throws Exception
+    {
+        // given
+        List<Category> categoryList = new ArrayList<>();
+        List<Product> productList = new ArrayList<>();
+
+        BigDecimal price = new BigDecimal("69.99");
+
+        Product product1 = new Product(
+                "19",
+                "Bloodborne",
+                "A souls like game",
+                price,
+                19,
+                "http://image_url",
+                true,
+                LocalDateTime.now(),
+                LocalDateTime.now()
+        );
+
+        Product product2 = new Product(
+                "19",
+                "The Last of Us",
+                "A narrative game with action sequences",
+                price,
+                19,
+                "http://image_url",
+                true,
+                LocalDateTime.now(),
+                LocalDateTime.now()
+        );
+
+        productList.add(product1);
+        productList.add(product2);
+
+        categoryList.add(new Category("11", "Video Games", productList));
+        categoryList.add(new Category("12", "Clothes", new ArrayList<>()));
+
+        given(categoryService.findAll()).willReturn(categoryList);
+
+        // when
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/categories"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").isArray())
+                .andExpect(jsonPath("$.length()").value(categoryList.size()))
+                .andExpect(jsonPath("$[0].id").value(equalTo("11")))
+                .andExpect(jsonPath("$[0].products").isArray())
+                .andExpect(jsonPath("$[0].products.length()").value(productList.size()))
+                .andExpect(jsonPath("$[0].products[0].id").value(equalTo("19")))
+//                .andExpect(jsonPath("$[0].products[0].price", Matchers.comparesEqualTo(price.doubleValue())))
+                .andExpect(jsonPath("$[0].products[0].price").value(equalTo(price)))
+                .andExpect(jsonPath("$[1].products").isArray())
+                .andExpect(jsonPath("$[1].products.length()").value(0));
         // then
 //        verify(categoryService, times(1)).findAll();
     }
