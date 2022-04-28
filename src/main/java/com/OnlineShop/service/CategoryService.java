b8218b26ce65d7f87c70ee7bdacfedf85ec57247
farbod-behnam <farbod.behnam@gmail.com>
2022-04-28 12:01:39 +0430

    Create CategoryService

---
+++ src/main/java/com/OnlineShop/service/CategoryService.java
@@ -0,0 +1,16 @@
+package com.OnlineShop.service;
+
+import com.OnlineShop.entity.Category;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CategoryService
+{
+    public List<Category> findAll()
+    {
+        return new ArrayList<>();
+    }
+}
