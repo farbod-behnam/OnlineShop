7d23558bbb6001d79cb3ada5a016c07ad9921aff
farbod-behnam <farbod.behnam@gmail.com>
2022-05-04 19:59:26 +0430

    Add AlreadyExistsException to exception package

---
+++ src/main/java/com/OnlineShop/exception/AlreadyExistsException.java
@@ -0,0 +1,30 @@
+package com.OnlineShop.exception;
+
+import com.OnlineShop.entity.Category;
+
+public class AlreadyExistsException extends RuntimeException
+{
+    public AlreadyExistsException()
+    {
+    }
+
+    public AlreadyExistsException(String message)
+    {
+        super(message);
+    }
+
+    public AlreadyExistsException(String message, Throwable cause)
+    {
+        super(message, cause);
+    }
+
+    public AlreadyExistsException(Throwable cause)
+    {
+        super(cause);
+    }
+
+    public AlreadyExistsException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace)
+    {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
