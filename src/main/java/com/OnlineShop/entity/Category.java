b45ae694beed1dfed24bd9639c886730323f6a14
farbod-behnam <farbod.behnam@gmail.com>
2022-05-07 11:47:45 +0430

    Change relationship between Product and Category

--- src/main/java/com/OnlineShop/entity/Category.java
+++ src/main/java/com/OnlineShop/entity/Category.java
@@ -21,18 +21,14 @@ public class Category
     @Pattern(regexp = "[a-zA-Z][a-zA-Z0-9 ]+", message = "name should only contain chars/digits")
     private String name;
 
-    @DBRef
-    private List<Product> products;
-
     public Category()
     {
     }
 
-    public Category(String id, String name, List<Product> products)
+    public Category(String id, String name)
     {
         this.id = id;
         this.name = name;
-        this.products = products;
     }
 
     public String getId()
@@ -55,39 +51,12 @@ public class Category
         this.name = name;
     }
 
-    public List<Product> getProducts()
-    {
-        return products;
-    }
-
-    public void setProducts(List<Product> products)
-    {
-        this.products = products;
-    }
-
-    public void addProduct(Product product)
-    {
-        if (this.products == null)
-            this.products = new ArrayList<>();
-
-        this.products.add(product);
-    }
-
-    public void removeProduct(Product product)
-    {
-        if (this.products == null)
-            return;
-
-        this.products.remove(product);
-    }
-
     @Override
     public String toString()
     {
         return "Category [" +
                 "id='" + id + '\'' +
                 ", name='" + name + '\'' +
-                ", products=" + products +
                 ']';
     }
 }
