e06d7fe071b2346594ea2bc34db9c005b9fa8684
farbod-behnam <farbod.behnam@gmail.com>
2022-05-08 12:29:34 +0430

    Add IProductService injection to ProductController class

--- src/main/java/com/OnlineShop/controller/ProductController.java
+++ src/main/java/com/OnlineShop/controller/ProductController.java
@@ -1,6 +1,8 @@
 package com.OnlineShop.controller;
 
 import com.OnlineShop.entity.Product;
+import com.OnlineShop.service.IProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,10 +16,17 @@ import java.util.List;
 @RequestMapping("/api/products")
 public class ProductController
 {
+    private final IProductService productService;
+
+    public ProductController(IProductService productService)
+    {
+        this.productService = productService;
+    }
+
     @GetMapping
     public ResponseEntity<List<Product>> getProducts()
     {
-        List<Product> products = new ArrayList<>();
+        List<Product> products = productService.getProducts();
         return new ResponseEntity<>(products, HttpStatus.OK);
     }
 }
