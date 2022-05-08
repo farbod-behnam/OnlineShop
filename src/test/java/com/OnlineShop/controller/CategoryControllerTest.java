6ee48dd65a61cf1d2345e507039332e6365013d9
farbod-behnam <farbod.behnam@gmail.com>
2022-05-08 10:21:01 +0430

    Remove setUp method since CategoryControllerTest does not need BigDecimal comparison

--- src/test/java/com/OnlineShop/controller/CategoryControllerTest.java
+++ src/test/java/com/OnlineShop/controller/CategoryControllerTest.java
@@ -48,43 +48,7 @@ public class CategoryControllerTest
     @MockBean
     private ICategoryService categoryService;
 
-    @BeforeEach
-    void setUp()
-    {
 
-        // -----------------------------------------------------
-        // for comparison of BigDecimal this config is necessary
-        // -----------------------------------------------------
-
-        final ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.enable(DeserializationFeature.USE_LONG_FOR_INTS);
-        objectMapper.enable(DeserializationFeature.USE_BIG_DECIMAL_FOR_FLOATS);
-
-        Configuration.setDefaults(new Configuration.Defaults() {
-
-            private final JsonProvider jsonProvider = new JacksonJsonProvider(objectMapper);
-            private final MappingProvider mappingProvider = new JacksonMappingProvider(objectMapper);
-
-            @Override
-            public JsonProvider jsonProvider() {
-                return jsonProvider;
-            }
-
-            @Override
-            public MappingProvider mappingProvider() {
-                return mappingProvider;
-            }
-
-            @Override
-            public Set<Option> options() {
-                return EnumSet.noneOf(Option.class);
-            }
-        });
-        // -----------------------------------------------------
-        // end of configuration
-        // -----------------------------------------------------
-
-    }
 
     @Test
     public void getCategories_returnsEmptyCategoryList() throws Exception
