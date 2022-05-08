f61d49476bc230972185a31efc26194c4b344e11
farbod-behnam <farbod.behnam@gmail.com>
2022-05-08 20:47:21 +0430

    Create IProductRepository interface and inject it into ProductService

--- src/main/java/com/OnlineShop/service/ProductService.java
+++ src/main/java/com/OnlineShop/service/ProductService.java
@@ -1,6 +1,8 @@
 package com.OnlineShop.service;
 
 import com.OnlineShop.entity.Product;
+import com.OnlineShop.repository.IProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,11 +12,19 @@ import java.util.List;
 @Service
 public class ProductService implements IProductService
 {
+    private final IProductRepository productRepository;
+
+    @Autowired
+    public ProductService(IProductRepository productRepository)
+    {
+        this.productRepository = productRepository;
+    }
+
     @Override
     @Transactional
     public List<Product> getProducts()
     {
-        return new ArrayList<>();
+        return productRepository.findAll();
     }
 
     @Override
