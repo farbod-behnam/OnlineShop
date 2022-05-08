a06758967062f89fef28ab4328bbc2dac1bf7d47
farbod-behnam <farbod.behnam@gmail.com>
2022-05-08 12:31:14 +0430

    Modify getProducts_shouldReturnProductList TDD method with IProductService

--- src/test/java/com/OnlineShop/controller/ProductControllerTest.java
+++ src/test/java/com/OnlineShop/controller/ProductControllerTest.java
@@ -1,5 +1,8 @@
 package com.OnlineShop.controller;
 
+import com.OnlineShop.entity.Category;
+import com.OnlineShop.entity.Product;
+import com.OnlineShop.service.IProductService;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jayway.jsonpath.Configuration;
@@ -13,13 +16,19 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.EnumSet;
+import java.util.List;
 import java.util.Set;
 
+import static org.hamcrest.Matchers.equalTo;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -32,6 +41,9 @@ class ProductControllerTest
 
     @Autowired
     private MockMvc mockMvc;
+
+    @MockBean
+    private IProductService productService;
 
     @BeforeEach
     void setUp()
@@ -75,14 +87,56 @@ class ProductControllerTest
     public void getProducts_shouldReturnProductList() throws Exception
     {
         // given
+        List<Product> productList = new ArrayList<>();
 
+        BigDecimal price = new BigDecimal("69.99");
+        Category category = new Category("11", "Video Games");
+
+        Product product1 = new Product(
+                "19",
+                "Bloodborne",
+                "A souls like game",
+                price,
+                19,
+                "http://image_url",
+                category,
+                true,
+                LocalDateTime.now(),
+                LocalDateTime.now()
+        );
+
+        Product product2 = new Product(
+                "19",
+                "The Last of Us",
+                "A narrative game with action sequences",
+                price,
+                19,
+                "http://image_url",
+                category,
+                true,
+                LocalDateTime.now(),
+                LocalDateTime.now()
+        );
+
+        productList.add(product1);
+        productList.add(product2);
+
+        given(productService.getProducts()).willReturn(productList);
 
         // when
-        mockMvc.perform(MockMvcRequestBuilders.get("/api/products"))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$").isArray());
 
         // then
-//        verify(categoryService, times(1)).findAll();
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/products"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").isArray())
+                .andExpect(jsonPath("$.length()").value(productList.size()))
+                .andExpect(jsonPath("$[0].id").value(equalTo("19")))
+                .andExpect(jsonPath("$[0].name").value(equalTo("Bloodborne")))
+                .andExpect(jsonPath("$[0].price").value(equalTo(new BigDecimal("69.99"))))
+                .andExpect(jsonPath("$[0].quantity").value(equalTo(19L)))
+                .andExpect(jsonPath("$[0].category.id").value(equalTo("11")))
+                .andExpect(jsonPath("$[0].category.name").value(equalTo("Video Games")))
+                .andExpect(jsonPath("$[0].active").value(equalTo(true)));
+
     }
 }
