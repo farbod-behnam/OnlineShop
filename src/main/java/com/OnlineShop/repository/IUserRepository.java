58dac4198ee8dd9c489430c247e54a8a29693935
farbod-behnam <farbod.behnam@gmail.com>
2022-05-16 12:40:21 +0430

    Add 3 new methods to IUserRepository

--- src/main/java/com/OnlineShop/repository/IUserRepository.java
+++ src/main/java/com/OnlineShop/repository/IUserRepository.java
@@ -8,4 +8,7 @@ import java.util.Optional;
 public interface IUserRepository extends MongoRepository<AppUser, String>
 {
     Optional<AppUser> findByUsername(String username);
+    Optional<AppUser> findByEmail(String email);
+    Optional<AppUser> findByPhoneNumber(String email);
+    Optional<AppUser> findByUsernameOrEmailOrPhoneNumber(String username, String email, String phoneNumber);
 }
