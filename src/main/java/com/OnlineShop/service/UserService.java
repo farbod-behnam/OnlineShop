22c848b9bc25d8b967a2c0fc682db6e977a511a6
farbod-behnam <farbod.behnam@gmail.com>
2022-05-16 11:21:51 +0430

    Add TDD for UserService.getUserById method

--- src/main/java/com/OnlineShop/service/UserService.java
+++ src/main/java/com/OnlineShop/service/UserService.java
@@ -1,12 +1,14 @@
 package com.OnlineShop.service;
 
 import com.OnlineShop.entity.AppUser;
+import com.OnlineShop.exception.NotFoundException;
 import com.OnlineShop.repository.IUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -30,7 +32,16 @@ public class UserService implements IUserService
     @Override
     public AppUser getUserById(String userId)
     {
-        return null;
+        Optional<AppUser> result = userRepository.findById(userId);
+
+        AppUser user;
+
+        if (result.isPresent())
+            user = result.get();
+        else
+            throw new NotFoundException("User with id: [" + userId + "] cannot be found");
+
+        return user;
     }
 
     @Override
--- src/test/java/com/OnlineShop/service/UserServiceTest.java
+++ src/test/java/com/OnlineShop/service/UserServiceTest.java
@@ -1,5 +1,11 @@
 package com.OnlineShop.service;
 
+import com.OnlineShop.entity.AppRole;
+import com.OnlineShop.entity.AppUser;
+import com.OnlineShop.entity.Country;
+import com.OnlineShop.enums.CountryEnum;
+import com.OnlineShop.enums.RoleEnum;
+import com.OnlineShop.exception.NotFoundException;
 import com.OnlineShop.repository.IUserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -8,7 +14,16 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
@@ -40,8 +55,53 @@ class UserServiceTest
     }
 
     @Test
-    void getUserById()
+    void getUserById_shouldReturnAnAppUser()
     {
+        // given
+        Set<AppRole> roles = new HashSet<>();
+
+        Country country = new Country("10", CountryEnum.Germany.name());
+        AppRole role = new AppRole("11", RoleEnum.ROLE_USER.name());
+
+        roles.add(role);
+
+        AppUser user = new AppUser(
+                "19",
+                "John",
+                "Wick",
+                "001666666666",
+                "john.wick@gmail.com",
+                roles,
+                "john.wick",
+                "password1234",
+                country,
+                "This is an address",
+                LocalDateTime.now(),
+                LocalDateTime.now()
+        );
+
+        given(userRepository.findById(anyString())).willReturn(Optional.of(user));
+
+        // when
+        AppUser foundUser = underTestUserService.getUserById(anyString());
+
+        // then
+        verify(userRepository).findById(anyString());
+        assertThat(foundUser).isEqualTo(user);
+    }
+
+    @Test
+    void getUserById_shouldThrowNotFoundException()
+    {
+        // given
+        String userId = "11";
+
+        // when
+
+        // then
+        assertThatThrownBy(() -> underTestUserService.getUserById(userId))
+                .isInstanceOf(NotFoundException.class)
+                .hasMessageContaining("User with id: [" + userId + "] cannot be found");
     }
 
     @Test
