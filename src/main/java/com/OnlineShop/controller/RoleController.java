cefc58fc3e100cc7a6df5e1b8abfee719c4aab99
farbod-behnam <farbod.behnam@gmail.com>
2022-05-18 11:47:25 +0430

    Create RoleController

---
+++ src/main/java/com/OnlineShop/controller/RoleController.java
@@ -0,0 +1,33 @@
+package com.OnlineShop.controller;
+
+import com.OnlineShop.entity.AppRole;
+import com.OnlineShop.service.IRoleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/roles")
+public class RoleController
+{
+    private final IRoleService roleService;
+
+    @Autowired
+    public RoleController(IRoleService roleService)
+    {
+        this.roleService = roleService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<AppRole>> getRoles()
+    {
+        List<AppRole> roles = roleService.getRoles();
+
+        return new ResponseEntity<>(roles, HttpStatus.OK);
+    }
+}
