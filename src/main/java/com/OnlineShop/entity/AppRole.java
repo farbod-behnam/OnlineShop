b96c2dc924de342ca6ee8a08525db4bc2de01d80
farbod-behnam <farbod.behnam@gmail.com>
2022-05-18 11:45:47 +0430

    Add validation to AppRole entity

--- src/main/java/com/OnlineShop/entity/AppRole.java
+++ src/main/java/com/OnlineShop/entity/AppRole.java
@@ -3,11 +3,19 @@ package com.OnlineShop.entity;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 @Document(collection = "Role")
 public class AppRole
 {
     @Id
     private String id;
+
+    @NotBlank(message = "name is required")
+    @Size(min = 6, max = 16, message = "name must be between 6 and 16 character")
+    @Pattern(regexp = "^[A-Z]+[_][A-Z]+$", message = "only upper case with one underscore between ex: ROLE_USER")
     private String name;
 
     public AppRole()
@@ -38,5 +46,14 @@ public class AppRole
     public void setName(String name)
     {
         this.name = name;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "AppRole [" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ']';
     }
 }
