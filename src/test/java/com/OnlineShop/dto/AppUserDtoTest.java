ba8dcb08d8d4a36cf159e6be808271b6bc566488
farbod-behnam <farbod.behnam@gmail.com>
2022-05-20 12:03:19 +0430

    Create AppUserDtoTest name validation TDD and remove its TDDs from AppUserTest

--- src/test/java/com/OnlineShop/dto/AppUserDtoTest.java
+++ src/test/java/com/OnlineShop/dto/AppUserDtoTest.java
@@ -18,7 +18,6 @@ import java.util.HashSet;
 import java.util.Set;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class AppUserDtoTest
 {
@@ -53,9 +52,7 @@ class AppUserDtoTest
                 "johnwick", // j.wick
                 "Password1234!",
                 countryId,
-                "Cecilia Chapman 711-2880 Nulla St. Mankato Mississippi 96522 (257) 563-7401",
-                LocalDateTime.now(),
-                LocalDateTime.now()
+                "Cecilia Chapman 711-2880 Nulla St. Mankato Mississippi 96522 (257) 563-7401"
         );
 
         // when
@@ -93,9 +90,7 @@ class AppUserDtoTest
                 "johnwick", // j.wick
                 "Password1234!",
                 countryId,
-                "Cecilia Chapman 711-2880 Nulla St. Mankato Mississippi 96522 (257) 563-7401",
-                LocalDateTime.now(),
-                LocalDateTime.now()
+                "Cecilia Chapman 711-2880 Nulla St. Mankato Mississippi 96522 (257) 563-7401"
         );
 
         // when
@@ -115,5 +110,137 @@ class AppUserDtoTest
         assertThat(violation.getMessage()).isEqualTo("first name is required");
         assertThat(violation.getPropertyPath().toString()).isEqualTo("firstName");
         assertThat(violation.getInvalidValue()).isEqualTo(null);
+    }
+
+    @Test
+    public void user_sizeNameLessThan3_shouldNotValidate()
+    {
+        // given
+        Set<String> roles = new HashSet<>();
+
+        String countryId = "10";
+        String roleId = "11";
+
+        roles.add(roleId);
+
+
+        AppUserDto user = new AppUserDto(
+                "19",
+                "Jo",
+                "Wick",
+                "0016666666666",
+                "john.wick@gmail.com",
+                roles,
+                "johnwick", // j.wick
+                "Password1234!",
+                countryId,
+                "Cecilia Chapman 711-2880 Nulla St. Mankato Mississippi 96522 (257) 563-7401"
+        );
+
+        // when
+
+        // then
+        Set<ConstraintViolation<AppUserDto>> violations = validator.validate(user);
+
+//        for (ConstraintViolation<AppUser> violation : violations)
+//        {
+//            System.out.println("Violation Message: " + violation.getMessage());
+//        }
+
+        assertThat(violations.isEmpty()).isFalse();
+        assertThat(violations.size()).isEqualTo(1);
+
+        ConstraintViolation<AppUserDto> violation = violations.iterator().next();
+        assertThat(violation.getMessage()).isEqualTo("first name must be between 3 and 25 character");
+        assertThat(violation.getPropertyPath().toString()).isEqualTo("firstName");
+        assertThat(violation.getInvalidValue()).isEqualTo("Jo");
+    }
+
+
+    @Test
+    public void user_sizeNameGreaterThan45_shouldNotValidate()
+    {
+        // given
+        Set<String> roles = new HashSet<>();
+
+        String countryId = "10";
+        String roleId = "11";
+
+        roles.add(roleId);
+
+
+        AppUserDto user = new AppUserDto(
+                "19",
+                "asdfasdfasdfgasdfasdfgasdf",
+                "Wick",
+                "0016666666666",
+                "john.wick@gmail.com",
+                roles,
+                "johnwick", // j.wick
+                "Password1234!",
+                countryId,
+                "Cecilia Chapman 711-2880 Nulla St. Mankato Mississippi 96522 (257) 563-7401"
+        );
+
+        // when
+
+        // then
+        Set<ConstraintViolation<AppUserDto>> violations = validator.validate(user);
+
+        for (ConstraintViolation<AppUserDto> violation : violations)
+        {
+            System.out.println("Violation Message: " + violation.getMessage());
+        }
+
+        assertThat(violations.isEmpty()).isFalse();
+        assertThat(violations.size()).isEqualTo(1);
+
+        ConstraintViolation<AppUserDto> violation = violations.iterator().next();
+        assertThat(violation.getMessage()).isEqualTo("first name must be between 3 and 25 character");
+        assertThat(violation.getPropertyPath().toString()).isEqualTo("firstName");
+        assertThat(violation.getInvalidValue()).isEqualTo("asdfasdfasdfgasdfasdfgasdf");
+    }
+
+    @Test
+    public void user_nameContainsSpecialCharactersOrNumbers_shouldNotValidate()
+    {
+        // given
+        Set<String> roles = new HashSet<>();
+
+        String countryId = "10";
+        String roleId = "11";
+
+        roles.add(roleId);
+
+        AppUserDto user = new AppUserDto(
+                "19",
+                "John @!",
+                "Wick",
+                "0016666666666",
+                "john.wick@gmail.com",
+                roles,
+                "johnwick", // j.wick
+                "Password1234!",
+                countryId,
+                "Cecilia Chapman 711-2880 Nulla St. Mankato Mississippi 96522 (257) 563-7401"
+        );
+
+        // when
+
+        // then
+        Set<ConstraintViolation<AppUserDto>> violations = validator.validate(user);
+
+        for (ConstraintViolation<AppUserDto> violation : violations)
+        {
+            System.out.println("Violation Message: " + violation.getMessage());
+        }
+
+        assertThat(violations.isEmpty()).isFalse();
+        assertThat(violations.size()).isEqualTo(1);
+
+        ConstraintViolation<AppUserDto> violation = violations.iterator().next();
+        assertThat(violation.getMessage()).isEqualTo("first name should only contain alphabet character");
+        assertThat(violation.getPropertyPath().toString()).isEqualTo("firstName");
+        assertThat(violation.getInvalidValue()).isEqualTo("John @!");
     }
 }
