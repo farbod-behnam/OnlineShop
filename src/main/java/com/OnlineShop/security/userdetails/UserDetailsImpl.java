b0a67cb1383508f925c0861ce50d10e46f416ab5
farbod-behnam <farbod.behnam@gmail.com>
2022-05-24 17:00:59 +0430

    Implement UserDetails interface

---
+++ src/main/java/com/OnlineShop/security/userdetails/UserDetailsImpl.java
@@ -0,0 +1,141 @@
+package com.OnlineShop.security.userdetails;
+
+import com.OnlineShop.entity.AppUser;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class UserDetailsImpl implements UserDetails
+{
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String email;
+    @JsonIgnore
+    private String password;
+    private Collection<GrantedAuthority> authorities;
+
+    public UserDetailsImpl(String id, String firstName, String lastName, String username, String email, String password, Collection<GrantedAuthority> authorities)
+    {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.authorities = authorities;
+    }
+
+    public String getId()
+    {
+        return id;
+    }
+
+    public String getFirstName()
+    {
+        return firstName;
+    }
+
+    public String getLastName()
+    {
+        return lastName;
+    }
+
+    public String getEmail()
+    {
+        return email;
+    }
+
+
+    @Override
+    public String getPassword()
+    {
+        return password;
+    }
+
+    @Override
+    public String getUsername()
+    {
+        return username;
+    }
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities()
+    {
+        return authorities;
+    }
+
+    @Override
+    public boolean isAccountNonExpired()
+    {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked()
+    {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired()
+    {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled()
+    {
+        return true;
+    }
+
+    public static UserDetailsImpl buildUserDetails(AppUser user)
+    {
+        List<GrantedAuthority> authorities = user.getRoles().stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName()))
+                .collect(Collectors.toList());
+
+        return new UserDetailsImpl(
+                user.getId(),
+                user.getFirstName(),
+                user.getLastName(),
+                user.getUsername(),
+                user.getEmail(),
+                user.getPassword(),
+                authorities
+        );
+    }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o) return true;
+        if (!(o instanceof UserDetailsImpl)) return false;
+
+        UserDetailsImpl that = (UserDetailsImpl) o;
+
+        if (!id.equals(that.id)) return false;
+        if (!username.equals(that.username)) return false;
+        if (!email.equals(that.email)) return false;
+        if (!password.equals(that.password)) return false;
+        return authorities.equals(that.authorities);
+    }
+
+    @Override
+    public int hashCode()
+    {
+        int result = id.hashCode();
+        result = 31 * result + username.hashCode();
+        result = 31 * result + email.hashCode();
+        result = 31 * result + password.hashCode();
+        result = 31 * result + authorities.hashCode();
+        return result;
+    }
+}
