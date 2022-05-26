a4c41724640c81e69da9e3208b950aa9985fe324
farbod-behnam <farbod.behnam@gmail.com>
2022-05-26 19:03:33 +0430

    Add AuthTokenFilter and authenticationManagerBean

--- src/main/java/com/OnlineShop/security/SecurityConfig.java
+++ src/main/java/com/OnlineShop/security/SecurityConfig.java
@@ -1,8 +1,10 @@
 package com.OnlineShop.security;
 
+import com.OnlineShop.filter.AuthTokenFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -11,6 +13,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import java.security.SecureRandom;
 
@@ -18,12 +21,14 @@ import java.security.SecureRandom;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter
 {
-    @Autowired
     private final UserDetailsService userDetailsService;
+    private final ITokenService tokenService;
 
-    public SecurityConfig(UserDetailsService userDetailsService)
+    @Autowired
+    public SecurityConfig(UserDetailsService userDetailsService, ITokenService tokenService)
     {
         this.userDetailsService = userDetailsService;
+        this.tokenService = tokenService;
     }
 
     @Bean
@@ -33,11 +38,25 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter
         return new BCryptPasswordEncoder(BCryptPasswordEncoder.BCryptVersion.$2Y, strength, new SecureRandom());
     }
 
+    @Bean
+    public AuthTokenFilter authenticationJwtTokenFilter()
+    {
+        return new AuthTokenFilter(tokenService);
+    }
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception
     {
         auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
     }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception
+    {
+        return super.authenticationManagerBean();
+    }
+
 
     @Override
     protected void configure(HttpSecurity http) throws Exception
@@ -47,5 +66,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter
         http.authorizeRequests().anyRequest().permitAll();
 
 //        http.addFilter(null);
+        http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 }
