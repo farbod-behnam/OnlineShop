3ea72c96c234caa58b391f98bb85851949a2e37f
farbod-behnam <farbod.behnam@gmail.com>
2022-06-12 20:59:36 +0430

    feat[order]: add GET method for getting all orders by admin

---
+++ src/main/java/com/OnlineShop/controller/OrderController.java
@@ -0,0 +1,35 @@
+package com.OnlineShop.controller;
+
+
+import com.OnlineShop.entity.order.Order;
+import com.OnlineShop.service.IOrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/orders")
+public class OrderController
+{
+    private final IOrderService orderService;
+
+    @Autowired
+    public OrderController(IOrderService orderService)
+    {
+        this.orderService = orderService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Order>> getOrders()
+    {
+        List<Order> orders = orderService.getOrders();
+
+        return new ResponseEntity<>(orders, HttpStatus.OK);
+    }
+
+
+
+}
