48874522fcacbfc37de26796d7a3e08007129ae2
farbod-behnam <farbod.behnam@gmail.com>
2022-06-13 13:15:58 +0430

    feat[order]: inject IOrderRepository into OrderService and implement get all orders method

--- src/main/java/com/OnlineShop/service/impl/OrderService.java
+++ src/main/java/com/OnlineShop/service/impl/OrderService.java
@@ -2,7 +2,9 @@ package com.OnlineShop.service.impl;
 
 import com.OnlineShop.dto.request.order.OrderRequest;
 import com.OnlineShop.entity.order.Order;
+import com.OnlineShop.repository.IOrderRepository;
 import com.OnlineShop.service.IOrderService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,10 +15,18 @@ import java.util.List;
 public class OrderService implements IOrderService
 {
 
+    private final IOrderRepository orderRepository;
+
+    @Autowired
+    public OrderService(IOrderRepository orderRepository)
+    {
+        this.orderRepository = orderRepository;
+    }
+
     @Override
     public List<Order> getOrders()
     {
-        return null;
+        return orderRepository.findAll();
     }
 
     @Override
