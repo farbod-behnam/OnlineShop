1b8c7e64f943bf80aafbe509bc4c01085a451482
farbod-behnam <farbod.behnam@gmail.com>
2022-06-14 16:11:47 +0430

    feat[order]: add findOrderByUserAndId method to IOrderRepository interface

--- src/main/java/com/OnlineShop/repository/IOrderRepository.java
+++ src/main/java/com/OnlineShop/repository/IOrderRepository.java
@@ -5,8 +5,10 @@ import com.OnlineShop.entity.order.Order;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface IOrderRepository extends MongoRepository<Order, String>
 {
     List<Order> findOrdersByUser(AppUser user);
+    Optional<Order> findOrderByUserAndId(AppUser user, String id);
 }
