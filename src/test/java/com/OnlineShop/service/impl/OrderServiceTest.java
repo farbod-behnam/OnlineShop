fc69b040b92b8351e18ea1ceeee374928b4d6a74
farbod-behnam <farbod.behnam@gmail.com>
2022-06-16 10:40:36 +0430

    feat[order]: add test for implementation of createUserOrder method

--- src/test/java/com/OnlineShop/service/impl/OrderServiceTest.java
+++ src/test/java/com/OnlineShop/service/impl/OrderServiceTest.java
@@ -1,5 +1,7 @@
 package com.OnlineShop.service.impl;
 
+import com.OnlineShop.dto.request.order.OrderItemRequest;
+import com.OnlineShop.dto.request.order.OrderRequest;
 import com.OnlineShop.entity.*;
 import com.OnlineShop.entity.order.Order;
 import com.OnlineShop.entity.order.OrderItem;
@@ -8,18 +10,15 @@ import com.OnlineShop.enums.RoleEnum;
 import com.OnlineShop.exception.NotFoundException;
 import com.OnlineShop.repository.IOrderRepository;
 import com.OnlineShop.service.IOrderService;
+import com.OnlineShop.service.IProductService;
 import com.OnlineShop.service.IUserService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -45,15 +44,15 @@ class OrderServiceTest
     @Mock
     private IUserService userService;
 
+    @Mock
+    private IProductService productService;
+
 
     @BeforeEach
     void setUp()
     {
         MockitoAnnotations.openMocks(this);
-        underTestOrderService = new OrderService(orderRepository, userService);
-
-
-
+        underTestOrderService = new OrderService(orderRepository, userService, productService);
     }
 
     @Test
@@ -470,7 +469,115 @@ class OrderServiceTest
     }
 
     @Test
-    void createUserOrder()
+    void createUserOrder_shouldReturnAnOrder()
     {
+        // given
+
+        // request
+        OrderItemRequest orderItemRequest1 = new OrderItemRequest("19", 9);
+//        OrderItemRequest orderItemRequest2 = new OrderItemRequest("20", 2);
+
+        List<OrderItemRequest> orderItemRequestList = new ArrayList<>();
+        orderItemRequestList.add(orderItemRequest1);
+//        orderItemRequestList.add(orderItemRequest2);
+
+        OrderRequest orderRequest = new OrderRequest(orderItemRequestList);
+
+        // created
+
+        BigDecimal price = new BigDecimal("69.99");
+        Category category = new Category("11", "Video Games");
+
+        Product product1 = new Product(
+                "19",
+                "Bloodborne",
+                "A souls like game",
+                price,
+                19,
+                "http://image_url",
+                category,
+                true,
+                LocalDateTime.now(),
+                LocalDateTime.now()
+        );
+
+        Product product2 = new Product(
+                "20",
+                "The Last of Us",
+                "A narrative game with action sequences",
+                price,
+                19,
+                "http://image_url",
+                category,
+                true,
+                LocalDateTime.now(),
+                LocalDateTime.now()
+        );
+
+        OrderItem item1 = new OrderItem(product1, 9);
+//        OrderItem item2 = new OrderItem(product1, 9);
+//        OrderItem item2 = new OrderItem(product2, 2);
+
+        List<OrderItem> orderItemList = new ArrayList<>();
+        orderItemList.add(item1);
+//        orderItemList.add(item2);
+
+        // user
+        Set<AppRole> roles = new HashSet<>();
+
+        Country country = new Country("10", CountryEnum.Germany.name());
+        AppRole role = new AppRole("11", RoleEnum.ROLE_USER.name());
+
+        roles.add(role);
+
+        AppUser user = new AppUser(
+                "19",
+                "John",
+                "Wick",
+                "001666666666",
+                "john.wick@gmail.com",
+                roles,
+                "john.wick",
+                "Password!1234",
+                country,
+                "This is an address",
+                LocalDateTime.now(),
+                LocalDateTime.now()
+        );
+
+        Order order = new Order(
+                "11",
+                orderItemList,
+                user,
+                false
+        );
+
+        given(productService.subtractProductQuantity(anyString(), any())).willReturn(product1);
+//        given(productService.subtractProductQuantity(anyString(), any())).willReturn(product2);
+
+        given(userService.getLoggedInUser()).willReturn(user);
+        given(orderRepository.save(any(Order.class))).willReturn(order);
+
+        // when
+        Order createdOrder = underTestOrderService.createUserOrder(orderRequest);
+
+        // then
+        ArgumentCaptor<Order> orderArgumentCaptor = ArgumentCaptor.forClass(Order.class);
+        verify(orderRepository).save(orderArgumentCaptor.capture());
+        Order capturedOrder = orderArgumentCaptor.getValue();
+
+        assertThat(capturedOrder.getId()).isEqualTo(null);
+        assertThat(capturedOrder.getOrderItemList().get(0).getProduct().getId()).isEqualTo(createdOrder.getOrderItemList().get(0).getProduct().getId());
+        assertThat(capturedOrder.getOrderItemList().get(0).getProduct().getName()).isEqualTo(createdOrder.getOrderItemList().get(0).getProduct().getName());
+        assertThat(capturedOrder.getOrderItemList().get(0).getProduct().getDescription()).isEqualTo(createdOrder.getOrderItemList().get(0).getProduct().getDescription());
+        assertThat(capturedOrder.getOrderItemList().get(0).getProduct().getPrice()).isEqualTo(createdOrder.getOrderItemList().get(0).getProduct().getPrice());
+        assertThat(capturedOrder.getOrderItemList().get(0).getProduct().getQuantity()).isEqualTo(createdOrder.getOrderItemList().get(0).getProduct().getQuantity());
+        assertThat(capturedOrder.getOrderItemList().get(0).getProduct().getImageUrl()).isEqualTo(createdOrder.getOrderItemList().get(0).getProduct().getImageUrl());
+        assertThat(capturedOrder.getOrderItemList().get(0).getProduct().getCategory()).isEqualTo(createdOrder.getOrderItemList().get(0).getProduct().getCategory());
+        assertThat(capturedOrder.getOrderItemList().get(0).getProduct().isActive()).isEqualTo(createdOrder.getOrderItemList().get(0).getProduct().isActive());
+        assertThat(capturedOrder.getUser()).isEqualTo(createdOrder.getUser());
+        assertThat(capturedOrder.getTotalPrice()).isEqualTo(createdOrder.getTotalPrice());
+        assertThat(capturedOrder.isPaymentSuccessful()).isEqualTo(createdOrder.isPaymentSuccessful());
+
     }
 }
