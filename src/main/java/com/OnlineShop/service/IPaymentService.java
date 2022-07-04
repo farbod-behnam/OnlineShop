593d3a3b14ff4cf429d4529bb334286e457ff8ec
farbod-behnam <farbod.behnam@gmail.com>
2022-07-04 14:03:22 +0430

    feat[payment]: Add IPaymentService interface which uses RabbitMQ

---
+++ src/main/java/com/OnlineShop/service/IPaymentService.java
@@ -0,0 +1,23 @@
+package com.OnlineShop.service;
+
+import com.OnlineShop.entity.AppUser;
+import com.OnlineShop.entity.order.Order;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface IPaymentService
+{
+    /**
+     * It sends the paymentOrderRequest to payment application and
+     * charges the wallet card of specified user based on the user id
+     * @param order {@link Order} argument for sending information to payment application
+     */
+    void chargeCard(Order order);
+
+    /**
+     * it sends the user to payment application for creating or updating
+     * the user table in payment application
+     * @param user {@link AppUser} argument for sending user information to payment application
+     */
+    void saveUserRecord(AppUser user);
+}
